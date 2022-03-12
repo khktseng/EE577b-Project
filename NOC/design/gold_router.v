286d823518186595f736e6a15770195400c1ed02
Kyle Tseng <kyletsen@viterbi-scf2.usc.edu>
2022-03-11 19:32:29 -0800

    created template NOC file

---
+++ NOC/design/gold_router.v
@@ -0,0 +1,20 @@
+// cardinal NOC verilog
+
+module gold_router(
+	input clk, reset,
+	output polarity,
+
+	input cwsi, ccwsi, pesi,
+	output cwri, ccwri, peri,
+	input [63:0] cwdi, ccwdi, pedi,
+
+	output cwso, ccwso, peso,
+	input cwro, ccwro, pero,
+	output [63:0] cwdo, ccwdo, pedo
+	);
+
+	
+
+
+
+endmodule
