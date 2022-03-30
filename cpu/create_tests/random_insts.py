cf9067d30ab20ba35cf3f0b94b43007df306f0f5
kyle <khktseng@gmail.com>
2022-03-29 17:32:46 -0700

    created some test files

--- cpu/create_tests/random_insts.py
+++ cpu/create_tests/random_insts.py
@@ -1,10 +1,14 @@
 from inst_helper import create_inst
 import random
 
-num_random = 100;
+num_random = 30;
 filename = 'inst_rand.fill'
 
 fill = open(filename, 'w')
+
+for i in range(1,32):
+    inst_hex = create_inst(19, rD=i, imm=i)
+    fill.write(inst_hex + '\n')
 
 for i in range(0, num_random):
     opnum = random.randint(1,18)
@@ -15,5 +19,9 @@ for i in range(0, num_random):
     inst_hex = create_inst(opnum, rD, rA, rB)
     fill.write(inst_hex + '\n')
 
-fill.write(create_inst())
+for i in range(0,32):
+    inst_hex = create_inst(20, rD=i, imm=i)
+    fill.write(inst_hex + '\n')
+
+fill.write(create_inst() + '\n')
 fill.close()
