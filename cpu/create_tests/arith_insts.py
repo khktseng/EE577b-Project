f4fc6e6b0df505ed2cce887b3c30914166eebba5
kyle <khktseng@gmail.com>
2022-04-09 20:17:51 -0700

    added mem fill files to cmp

--- cpu/create_tests/arith_insts.py
+++ cpu/create_tests/arith_insts.py
@@ -8,9 +8,9 @@ for i in range(1,32):
     inst_hex = create_inst(19, rD=i, imm=i)
     fill.write(inst_hex)
 
-fill.write(create_inst(6,5,4))
-fill.write(create_inst(7,6,5))
-fill.write(create_inst(2,3,4))
+fill.write(create_inst(6, 6,5,4))
+fill.write(create_inst(6, 7,6,5))
+fill.write(create_inst(6, 2,3,4))
 
 for i in range(0,32):
     inst_hex = create_inst(20, rD=i, imm=i)
